df3b5a3091e07ee40d6e092909a0daab9dac86eb
TitliBasu37 <48563702+TitliBasu37@users.noreply.github.com>
2023-10-12 19:10:47 +0530

    Update objkey.py
    removing colons

--- Challenge3/objkey.py
+++ Challenge3/objkey.py
@@ -1,22 +1,12 @@
-# We have a nested object, we would like a function that you pass in the object and a key and get back the value. How this is implemented is up to you.
-##
-# Example Inputs
-# object = {“a”:{“b”:{“c”:”d”}}}
-# key = a/b/c
-##
-# object = {“x”:{“y”:{“z”:”a”}}}
-# key = x/y/z
-# value = a
-
-def getKey(obj: dict):
+def getKey(obj):
     keys = list(obj)
     if len(keys) != 1:
         raise Exception('either multiple keys or empty dict found')
     else:
         return keys[0]
+    
 
-
-def getNestedValue(obj: dict, key: str, isFound = False):
+def getNestedValue(obj, key, isFound = False):
     # print(obj, key, isFound)
     if type(obj) is not dict and not isFound:
         return None
